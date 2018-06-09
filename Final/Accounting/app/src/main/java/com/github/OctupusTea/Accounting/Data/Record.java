cc09dfb60fa6f6a00d502bd4c4e721cd86531116
OctupusTea <eland.tea@gmail.com>
2018-06-10 02:58:56 +0800

    Record Constructor
    Implement Record class constructor
    correcting spelling error: "Categoray" -> "Category"
    Some minor IDE / project changes

--- Final/Accounting/app/src/main/java/com/github/OctupusTea/Accounting/Data/Record.java
+++ Final/Accounting/app/src/main/java/com/github/OctupusTea/Accounting/Data/Record.java
@@ -1,21 +1,59 @@
-package com.github.OctupusTea.Accounting.Record;
+package com.github.OctupusTea.Accounting.Data;
 
+import java.lang.ref.SoftReference;
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class Record {
     private int id;
     private String accountName;
-    private String categorayName;
+    private String categoryName;
     private String date;
     private String currencyType;
     private double cost;
 
-    public Record() {
-        setDate();
+    public Record( )
+    {
+        this.Init( 0, "","",getDate( ),"",0.0 );
     }
 
-    public int getId() {
+    public Record( String accountName, String categoryName )
+    {
+        this.Init( 0, accountName, categoryName, getDate( ), "", 0.0 );
+    }
+
+    public Record( String accountName, String categoryName, double cost )
+    {
+        this.Init( 0, accountName, categoryName, getDate( ), "", cost );
+    }
+
+    public Record( String accountName, String categoryName, String date )
+    {
+        this.Init( 0, accountName, categoryName, date, "", 0.0 );
+    }
+
+    public Record( String accountName, String categoryName, String date, double cost )
+    {
+        this.Init( 0, accountName, categoryName, date, "", cost );
+    }
+
+    public Record( int id, String accountName, String categoryName, String date, double cost )
+    {
+        this.Init( id, accountName, categoryName, date, "", cost );
+    }
+
+    private void Init( int id, String accountName, String categoryName, String date, String currencyType, double cost )
+    {
+        this.id = id;
+        this.accountName = accountName;
+        this.categoryName = categoryName;
+        this.date = date;
+        this.currencyType = currencyType;
+        this.cost = cost;
+    }
+
+    public int getId( ) {
         return id;
     }
 
@@ -23,49 +61,60 @@ public class Record {
         this.id = id;
     }
 
-    public String getAccountName() {
+    public String getAccountName( ) {
         return accountName;
     }
 
-    public void setAccountName(String accountName) {
+    public void setAccountName( String accountName ) {
         this.accountName = accountName;
     }
 
-    public String getCategorayName() {
-        return categorayName;
+    public String getCategoryName( ) {
+        return categoryName;
     }
 
-    public void setCategorayName(String categorayName) {
-        this.categorayName = categorayName;
+    public void setCategoryName( String categorayName ) {
+        this.categoryName = categorayName;
     }
 
-    private void setDate() {
+    private void setDate( ) {
         Date date = new Date();
         SimpleDateFormat timeFormating = new SimpleDateFormat("yyyy-MM-dd");
         this.date = timeFormating.format(date);
     }
 
-    public void setDate(String date) {
+    public void setDate( String date )
+    {
+        SimpleDateFormat dateValidator = new SimpleDateFormat( "yyyy-MM-dd" );
+        try
+        {
+            dateValidator.parse( date );
+        }
+        catch( ParseException e ) // if ill-formated  date string
+        {
+            return;
+        }
+
         this.date = date;
     }
 
-    public String getDate() {
+    public String getDate( ) {
         return this.date;
     }
 
-    public String getCurrencyType() {
+    public String getCurrencyType( ) {
         return currencyType;
     }
 
-    public void setCurrencyType(String currencyType) {
+    public void setCurrencyType( String currencyType ) {
         this.currencyType = currencyType;
     }
 
-    public double getCost() {
+    public double getCost( ) {
         return cost;
     }
 
-    public void setCost(double cost) {
+    public void setCost( double cost ) {
         this.cost = cost;
     }
 
