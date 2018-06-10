4b39979cb9b8a877696d9404ddab7217e733a05a
OctupusTea <eland.tea@gmail.com>
2018-06-11 02:13:14 +0800

    Merge Activity From Remote

--- Final/Accounting/app/src/main/java/com/github/OctupusTea/Accounting/MainActivity.java
+++ Final/Accounting/app/src/main/java/com/github/OctupusTea/Accounting/MainActivity.java
@@ -1,31 +1,114 @@
-package com.github.OctupusTea.Accounting;
+package com.accounting1;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.Toast;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
-import com.accounting.R;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
+    Calendar calendar = Calendar.getInstance();
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
-		Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-		setSupportActionBar(toolbar);
+    public void arrow (int direction){    //點擊箭頭，調整日期顯示
+        SimpleDateFormat formatter2 = new SimpleDateFormat("yyyy/MM/dd");
+        calendar.add(Calendar.DATE, direction);
+        String Ago = formatter2.format(calendar.getTime());
+        TextView textView = (TextView) findViewById(R.id.textView);
+        textView.setText(Ago);
+    }
 
-		FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-		fab.setOnClickListener(new View.OnClickListener() {
-			@Override
-			public void onClick(View view) {
-				Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-						.setAction("Action", null).show();
-			}
-		});
-	}
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        ImageButton iBtn01 = (ImageButton) findViewById(R.id.imageButton);  //主頁面左按鍵
+        ImageButton iBtn02 = (ImageButton) findViewById(R.id.imageButton1); //主頁面右按鍵
+
+        SimpleDateFormat formatter = new SimpleDateFormat("yyyy/MM/dd");
+        Date curDate = new Date(System.currentTimeMillis()); //取得系統時間
+        String str = formatter.format(curDate);
+        TextView textView = (TextView) findViewById(R.id.textView);
+        textView.setText(str);
+        iBtn01.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                arrow(-1);
+            }
+        });
+        iBtn02.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                arrow(1);
+            }
+        });
+
+        Button categoryViewBtn = (Button) findViewById(R.id.button);
+        categoryViewBtn.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setClass(MainActivity.this, categoryView.class);
+                startActivity(intent);
+            }
+        });
+
+        Button accountViewBtn = (Button) findViewById(R.id.button1);
+        accountViewBtn.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setClass(MainActivity.this, accountview.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+
+
+    @Override  // menu UI
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override   // 觸發 menu
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
+
+
 
 }
---
+++ Final/Accounting/app/src/main/java/com/github/OctupusTea/Accounting/accountview.java
@@ -0,0 +1,65 @@
+package com.accounting1;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
+
+public class accountview extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_accountview);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        Button categoryViewBtn10 = (Button) findViewById(R.id.button10);
+        Button categoryViewBtn11 = (Button) findViewById(R.id.button11);
+        Button categoryViewBtn19 = (Button) findViewById(R.id.button19);
+        Button categoryViewBtn20 = (Button) findViewById(R.id.button20);
+        categoryViewBtn10.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setClass(accountview.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+        categoryViewBtn11.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setClass(accountview.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+        categoryViewBtn19.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setClass(accountview.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+        categoryViewBtn20.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setClass(accountview.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
+    }
+
+}
