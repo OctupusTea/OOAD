d95b4fe5a3dfe8ded58d3f70b66825c00fb8ac7b
OctupusTea <eland.tea@gmail.com>
2018-06-24 15:49:01 +0800

    (Small Changes)

--- Final/Accounting/app/src/main/java/com/github/OctupusTea/Accounting/passwordsetting.java
+++ Final/Accounting/app/src/main/java/com/github/OctupusTea/Accounting/passwordsetting.java
@@ -7,7 +7,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
-public class passwordsetting extends AppCompatActivity {
+public class passwordSetting extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
